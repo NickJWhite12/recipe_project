45c3ac0731b7739e616b2ca2454103fcd4c17157
NickJWhite12 <48346435+NickJWhite12@users.noreply.github.com>
2020-08-25 16:44:36 -0400

    Spring Project init

---
+++ src/test/java/com/nickjwhite12/recipe_project/RecipeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nickjwhite12.recipe_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
